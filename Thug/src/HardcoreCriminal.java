415abe7c4d4bac6756f726d001d082e08a9047e2
mobilevisuals <eyvind4@gmail.com>
2018-12-18 13:42:43 +0100

    z

--- Thug/src/HardcoreCriminal.java
+++ Thug/src/HardcoreCriminal.java
@@ -3,7 +3,7 @@ public class HardcoreCriminal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("gangsta");
+		System.out.println("gangstaz");
 
 	}
 
